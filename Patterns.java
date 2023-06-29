2e7dd696b271b15f7be2031424940fb0b5244199
Akash Gaur <134853842+akgaur12@users.noreply.github.com>
2023-06-29 09:45:51 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -91,6 +91,17 @@ public class Patterns
     }
     
     // ---------------------------------------------------------------------------------------------------------------------------
+    public static void halfPyramid(int n)
+    {
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+    
+
 
     
 }
