122c0fc503dc8c43984e6d47b5088ced57ab3372
Akash Gaur <134853842+akgaur12@users.noreply.github.com>
2023-06-29 09:48:15 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -69,7 +69,8 @@ public class Patterns
     // ---------------------------------------------------------------------------------------------------------------------------
     public static void solidRectangle(int r, int c)
     {
-        for(int i=0; i<r; i++){
+        for(int i=0; i<r; i++)
+        {
             for(int j=0; j<c; j++)
                 System.out.print("* ");
             System.out.println("");
@@ -79,8 +80,10 @@ public class Patterns
     // ---------------------------------------------------------------------------------------------------------------------------
     public static void holloRectangle(int r, int c)
     {
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
+        for(int i=0; i<r; i++)
+        {
+            for(int j=0; j<c; j++)
+            {
                 if(i==0 || j==0 || j==c-1 || i==r-1)
                     System.out.print("* ");
                 else
@@ -93,10 +96,11 @@ public class Patterns
     // ---------------------------------------------------------------------------------------------------------------------------
     public static void halfPyramid(int n)
     {
-        for(int i=0; i<n; i++){
-            for(int j=0; j<=i; j++){
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<=i; j++)
                 System.out.print("* ");
-            }
+            
             System.out.println("");
         }
     }
@@ -104,13 +108,33 @@ public class Patterns
     // ---------------------------------------------------------------------------------------------------------------------------
     public static void invertedHalfPyramid(int n)
     {
-        for(int i=0; i<n; i++){
-            for(int j=n; j>i; j--){
+        for(int i=0; i<n; i++)
+        {
+            for(int j=n; j>i; j--)
                 System.out.print("* ");
-            }
+            
             System.out.println("");
         }
     }
+
+    // ---------------------------------------------------------------------------------------------------------------------------
+    public static void rightHalfPyramid(int n)
+    {
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n-i; ++j)
+                System.out.print(" ");
+            
+            for(int j=1; j<=i; ++j)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+    
+    // ---------------------------------------------------------------------------------------------------------------------------
+
+        
+    
     
 
 
