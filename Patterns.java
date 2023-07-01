518e3cc8a0e9c18e4df816e7debd42e507699ca0
Akash Gaur <134853842+akgaur12@users.noreply.github.com>
2023-07-01 16:04:28 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -251,21 +251,30 @@ public class Patterns
     }
 
     // ---------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-        
+    public static void diamondPattern(int n)
+    {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++)
+                System.out.print(" ");
+            
+            for(int j=i; j>0; j--)
+                System.out.print("* ");
+               
+            System.out.println("");
+        }
+        for(int i=n-1; i>0; i--){
+            for(int j=1; j<=n-i; j++)
+                System.out.print(" ");
+            
+            for(int j=i; j>0; j--)
+                System.out.print("* ");
+                
+            System.out.println("");
+        }
+    }
     
     
-
-
-
-
-
-
-    
-}
+} 
 
     
 
