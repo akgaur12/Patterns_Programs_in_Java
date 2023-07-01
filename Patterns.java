b329823497504cb1e8407de8e1e09feee011dde2
Akash Gaur <134853842+akgaur12@users.noreply.github.com>
2023-07-01 16:01:36 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -234,6 +234,23 @@ public class Patterns
     }
 
     // ---------------------------------------------------------------------------------------------------------------------------
+    public static void palindromicNumberPyramid(int n)
+    {   
+        for(int i=1; i<=n; ++i){
+            for(int j=1; j<=n-i; ++j)
+                System.out.print(" ");
+            
+            for(int j=i; j>0; --j)
+                System.out.print(j);
+            
+            for(int j=2; j<=i; ++j)
+                System.out.print(j);
+       
+            System.out.println(""); 
+        }
+    }
+
+    // ---------------------------------------------------------------------------------------------------------------------------
 
 
 
